5f3d78d072d0427f75903ea5ca48f57496371d10
Okv <113612516+okvokv@users.noreply.github.com>
2023-03-23 11:35:40 +0300

    Delete components directory

--- components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom';
-//функция перенаправления в зависимости от состояния индикатора входа пользователя
-function ProtectedRoute({ element: Component, ...props }) {
-	return (
-		props.loggedIn ? <Component {...props} /> : <Navigate to='/sign-in' replace />
-	)
-};
-
-export default ProtectedRoute;
