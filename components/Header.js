1acc55018026541e000d1bcc0a86046bf177a9c5
Okv <113612516+okvokv@users.noreply.github.com>
2023-03-23 11:35:05 +0300

    Add files via upload

---
+++ components/Header.js
@@ -0,0 +1,25 @@
+import logo from '../images/logo.svg';
+
+//Секция заголовок =============================================================
+function Header(props) {
+
+	//промежуточная функция обработки клика
+	function handleClick() {
+		props.loggedIn ? props.onLogOut() : props.onTogglePage();
+	};
+
+	return (
+		<header className="header">
+
+			<img className="header__logo" src={logo} alt="Логотип" />
+
+			<div className="header__group">
+				<h3 className="header__email">{props.loggedIn && props.userEmail}</h3>
+				<h3 className="header__link" onClick={handleClick}>{(props.loggedIn && 'Выйти') || props.btnText}</h3>
+			</div>
+
+		</header>
+	);
+};
+
+export default Header;
